d2fece5878e3d4783c11f407185c5f654a43d272
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2020-01-10 22:56:25 +0100

    added authentication to usage endpoint

--- src/main/kotlin/org/bjartek/graphql/query/UsageQuery.kt
+++ src/main/kotlin/org/bjartek/graphql/query/UsageQuery.kt
@@ -2,17 +2,31 @@ package org.bjartek.graphql.query
 
 import com.expediagroup.graphql.annotations.GraphQLDescription
 import com.expediagroup.graphql.spring.operations.Query
+import graphql.schema.DataFetchingEnvironment
+import org.bjartek.graphql.MyGraphQLContext
 import org.bjartek.graphql.UsageInstrumentation
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.time.Instant
 
+fun DataFetchingEnvironment.isUser(expectedUser:String) : Boolean {
+    val user = this.getContext<MyGraphQLContext>().user
+    return user == expectedUser
+}
+
 @Component
-class UsageQuery(private val goboInstrumentation: UsageInstrumentation) : Query {
+class UsageQuery(
+        private val goboInstrumentation: UsageInstrumentation,
+        @Value("\${org.bjartek.usage.user:graphql}") val expectedUser: String
+) : Query {
 
     private val startTime = Instant.now()
 
     @GraphQLDescription("query for usage of this graphql api")
-    fun usage(): Usage {
+    fun usage(dataFetchingEnvironment: DataFetchingEnvironment): Usage {
+        if(!dataFetchingEnvironment.isUser(expectedUser)) {
+            throw RuntimeException("Not authorized")
+        }
         val fields = goboInstrumentation.fieldUsage.fields.map { UsageField(it.key, it.value.sum()) }
         val users = goboInstrumentation.userUsage.users.map { UserUsage(it.key, it.value.sum()) }
         return Usage(startTime.toString(), fields, users)
